90272ba75a00fc714a6a4d776c9096ba0d216f50
J Z <906641+jzischkau@users.noreply.github.com>
2018-03-10 18:18:03 -0500

    Static resources: major refactor

--- src/main/resources/static/assets/js/interface.js
+++ src/main/resources/static/assets/js/interface.js
@@ -1,10 +1,73 @@
 $(function () {
-  function showActivity(title) {
+  // figure out how much room we have for graph
+  var height = window.innerHeight;
+  var width = $('body').width() - $('#interface')[0].getBoundingClientRect().width;
+  $('#graph').height(height);
+  $('#interface').height(height-20);
+  var GRAPH = new Graph(width,height);
+  GRAPH.load();
+
+  $('#reset').click(function() {
+    GRAPH.resetPosition();
+  });
+  $('#zoom-in').click(function() {
+    GRAPH.zoom(1);
+  });
+  $('#zoom-out').click(function() {
+    GRAPH.zoom(-1);
+  });
+  $('#move-left').click(function() {
+    GRAPH.move('x',1);
+  });
+  $('#move-right').click(function() {
+    GRAPH.move('x',-1);
+  });
+  $('#move-up').click(function() {
+    GRAPH.move('y',1);
+  });
+  $('#move-down').click(function() {
+    GRAPH.move('y',-1);
+  });
+  
+  // highlight the selected node
+  function highlight(title) {
+    var $node = $('circle#hamgraph_'+title.replace(/ /g,'_'));
+    if($node.hasClass('selected')) {
+      $node.toggleClass('selected');
+      GRAPH.centerOnNode(); // this resets the centering
+    }
+    else {
+      $('circle').removeClass('selected');
+      $node.addClass('selected');
+    }
+  }
+
+  function selectActivityFromSearchResults(title) {
+    var node = $('circle#hamgraph_'+title.replace(/ /g,'_'));
+    if(node && node.length) {
+      // workaround to trigger d3 click via jquery
+      node.each(function(i,e) {
+        var evt = new MouseEvent('click');
+        e.dispatchEvent(evt);
+      });
+    }
+    else {
+      showDetails(title);
+    }
+  }
+
+  function selectActivityFromNodeClick(e,node) {
+    showDetails(node.title);
+    highlight(node.title);
+  }
+
+  function showDetails(title) {
+    /* load additional details for the activity into the details pane */
     $.get("/activities/search/findByTitle?title=" + encodeURIComponent(title), function (data) {
       if (!data ) return; //  || !data["_embedded"].movies) return;
       var activity = data; // ["_embedded"].movies[0];
-      $("#title").text(activity.title);
-      $("#description").text(activity.description);
+      $("#detail-title").text(activity.title);
+      $("#detail-description").text(activity.description);
       /*
       var $list = $("#participants").empty();
       activity.participants.forEach(function (participants) {
@@ -18,27 +81,33 @@ $(function () {
     return false;
   }
   function search() {
-    var query=$("#search").find("input[name=search]").val();
+    var query=$("#search").val();
     $.get("/activities/search/findByTitleLike?title=*" + encodeURIComponent(query) + "*", function (data) {
-      var t = $("table#results tbody").empty();
+      var resultsContainer = $("#search-results").empty();
       if (!data) return;
       data = data["_embedded"].activities;
       data.forEach(function (activity) {
-        $("<tr><td class='activity'>" + activity.title + "</td><td>" + activity.description + "</td></tr>").appendTo(t)
-        .click(function() { showActivity($(this).find("td.activity").text());})
+        var template = '<div class="search-result"><div class="title">'+activity.title+'</div><div class="description">'+activity.description+'</div></div>';
+        $(template).appendTo(resultsContainer).click(function() { 
+          var $this = $(this)
+          if($this.hasClass('selected')) {
+            $this.removeClass('selected');
+          }
+          else {
+            $('.search-result').removeClass('selected');
+            $this.addClass('selected');
+          }
+          selectActivityFromSearchResults($this.find(".title").text());
+        })
       });
-      showActivity(data[0].title);
+      selectActivityFromSearchResults(data[0].title);
     }, "json");
     return false;
   }
 
+  $('#interface').on('selectActivityFromNodeClick',selectActivityFromNodeClick);
   $("#search").submit(search);
+  $("#search-button").click(search);
   search();
 
-  // WIP
-  // figure out how much room we have for graph
-  var height = window.innerHeight - 100 - $('#interface')[0].getBoundingClientRect().height;
-  var width = $('body').width();
-  $('#graph').height(height);
-  loadGraph(width,height);
 });
