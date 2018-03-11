03cf4825705fe7912db9c19e2f6cea8cd9f01094
J Z <906641+jzischkau@users.noreply.github.com>
2018-03-11 14:43:49 -0400

    static: Add page for adding activities

---
+++ src/main/resources/static/assets/js/add.js
@@ -0,0 +1,34 @@
+$(function () {
+  // Load list of activities and populate the related dropdown
+  $.get('/activities', function(data) {
+      if (!data || !data._embedded || !data._embedded.activities) return;
+      data = data["_embedded"].activities;
+      $related = $('#related');
+      data.forEach(function(activity) {
+        $related.append('<option id="'+activity.title+'">'+activity.title+'</option>');
+      });
+  });
+
+  $('#add-activity-form').submit(function() {
+    var title = $('#title').val();
+    var description = $('#description').val();
+    if(!title || !description) {
+      alert('Must specify title and description');
+      return false;
+    }
+    var related = $('#related').val();
+    $.ajax({
+      type: 'POST',
+      url: '/activities',
+      contentType: 'application/json',
+      data: JSON.stringify({title: title, description: description})
+    })
+    .then(function() {
+      related.forEach(function(relatedActivity) {
+        $.get('/addRelationship?a1Title='+title+'&a2Title='+relatedActivity);
+      });
+    });
+    return false;
+  });
+
+});
