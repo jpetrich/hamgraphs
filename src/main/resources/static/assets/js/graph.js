cdbc720aad1273f7384be64b8872059eb0b55ff2
J Z <906641+jzischkau@users.noreply.github.com>
2018-03-11 01:06:07 -0500

    static resourcs: graph: separate render and data loading

--- src/main/resources/static/assets/js/graph.js
+++ src/main/resources/static/assets/js/graph.js
@@ -4,10 +4,6 @@ function Graph(width,height) {
   this.currentCenter = { x: 0, y: 0};
   this.currentScale = 1;
   //this.force = d3.layout.force().charge(-200).size([width, height]);
-  this.simulation = d3.forceSimulation()
-                      .stop()
-                      .force('charge',d3.forceManyBody(-200))
-                      .force('center',d3.forceCenter((this.width/2),(this.height/2)));
 
   this.svg = d3.select("#graph")
                .append("svg")
@@ -36,6 +32,7 @@ Graph.prototype.centerOnNode = function(node) {
   this.updatePosition();
 }
 
+// move the graph view
 Graph.prototype.move = function(axis,direction) {
   if(axis !== 'x' && axis !== 'y') {
     return;
@@ -61,73 +58,93 @@ Graph.prototype.updatePosition = function() {
   var x = this.currentCenter.x;
   var y = this.currentCenter.y;
   var s = this.currentScale;
-  this.svg.attr('style','transform:translateX('+x+'px) translateY('+y+'px) scale('+s+');');
+  this.svg.attr('style','transform-origin: center; transform:translateX('+x+'px) translateY('+y+'px) scale('+s+');');
+}
+
+Graph.prototype.render = function() {
+  var self = this;
+  if(this.renderInProgress) {
+    return;
+  }
+  this.renderInProgress = true;
+  var svg = this.svg;
+  var graph = this.data;
+  var linkDistance = this.linkDistance || graph.nodes.reduce(function(acc,node) {
+    return Math.max(acc,node.title.length*20);
+  }, 120);
+  this.linkDistance = linkDistance;
+  var simulation = this.simulation || d3.forceSimulation()
+                                        .nodes(graph.nodes)
+                                        .force('charge',d3.forceManyBody(-200))
+                                        .force('center',d3.forceCenter((this.width/2),(this.height/2)))
+                                        .force('links',d3.forceLink(graph.links).distance(linkDistance));
+
+  if(!this.simulation) {
+    this.simulation = simulation;
+  }
+  if(this.simulation.alpha() < 1) {
+    this.simulation.alpha(1);
+    this.simulation.restart();
+  }
+
+  var link = svg.selectAll(".link")
+                .data(graph.links).enter()
+                .append("line").attr("class", "link");
+
+  var node = svg.selectAll(".node")
+                .data(graph.nodes).enter()
+                .append("circle")
+                .attr("class", function (d) { return "node "+d.label })
+                .attr("id", function (d) { return 'hamgraph_'+d.title.replace(/ /g,'_') })
+                .attr("data-text", function (d) { return d.title })
+                .attr("r", function(d) { return d.title.length*7 });
+
+  node.on('click',function(e) {
+    self.centerOnNode(e);
+    $('#interface').trigger('selectActivityFromNodeClick',e);
+  });
+
+  var label = svg.selectAll('.graph-label')
+                .data(graph.nodes).enter()
+                .append('text')
+                .attr('class','graph-label')
+                .text(function(d){return d.title});
+
+  label.on('click',function(e) {
+    self.centerOnNode(e);
+    $('#interface').trigger('selectActivityFromNodeClick',e);
+  });
+
+  // force feed algo ticks
+  simulation.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+
+    label.attr('x',function(n) { return n.x - (n.title.length * 5); })
+        .attr('y',function(n) { return n.y + 4; });
+  });
+
+  simulation.on('end',function() {
+    self.renderInProgress = false;
+  });
+
 }
 
 Graph.prototype.load = function() {
   var self = this;
-  var svg = this.svg;
-  var simulation = this.simulation;
   var loadedPromise = new Promise(function(resolve,reject) {
     d3.json("/graph", function(error, graph) {
+      self.data = graph;
       if (error) {
         return reject(error);
       }
-
-      var linkDistance = graph.nodes.reduce(function(acc,node) {
-        return Math.max(acc,node.title.length*20);
-      }, 120);
-
-      //force.linkDistance(linkDistance);
-      simulation.nodes(graph.nodes);
-      simulation.force('links',d3.forceLink(graph.links).distance(linkDistance));
-      simulation.restart();
-      //force.nodes(graph.nodes).links(graph.links).start();
-
-      var link = svg.selectAll(".link")
-                    .data(graph.links).enter()
-                    .append("line").attr("class", "link");
-
-      var node = svg.selectAll(".node")
-                    .data(graph.nodes).enter()
-                    .append("circle")
-                    .attr("class", function (d) { return "node "+d.label })
-                    .attr("id", function (d) { return 'hamgraph_'+d.title.replace(/ /g,'_') })
-                    .attr("data-text", function (d) { return d.title })
-                    .attr("r", function(d) { return d.title.length*7 });
-                    //.call(force.drag);
-
-      node.on('click',function(e) {
-        self.centerOnNode(e);
-        $('#interface').trigger('selectActivityFromNodeClick',e);
-      });
-
-      var label = svg.selectAll('.graph-label')
-                    .data(graph.nodes).enter()
-                    .append('text')
-                    .attr('class','graph-label')
-                    .text(function(d){return d.title});
-      
-      label.on('click',function(e) {
-        self.centerOnNode(e);
-        $('#interface').trigger('selectActivityFromNodeClick',e);
-      });
-
-      // force feed algo ticks
-      simulation.on("tick", function() {
-        link.attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        node.attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-
-        label.attr('x',function(n) { return n.x - (n.title.length * 5); })
-            .attr('y',function(n) { return n.y + 4; });
-      });
-
-      simulation.on('end',resolve);
+      self.render();
+      resolve();
     });
   });
   return loadedPromise;
